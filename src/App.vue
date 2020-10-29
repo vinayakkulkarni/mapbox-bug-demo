c385a4f07bfbc5a00871b68166bff62823aa2b87
Kok Wai Gie <woshikie@gmail.com>
2020-10-29 18:21:52 +0800

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <MglMap class="mglmap" container="map" :zoom="10.5" :access-token="accessToken" :map-style="mapStyle" :center="sgCenter">
+    </MglMap>
+  </div>
+</template>
+
+<script>
+import 'mapbox-gl/dist/mapbox-gl.css'
+// import Mapbox from "mapbox-gl"
+import { MglMap } from "v-mapbox"
+
+export default {
+  name: 'App',
+  components: {
+    MglMap
+  },
+  data(){
+    return {
+      accessToken: '', // Enter your own access token
+      mapStyle: 'mapbox://styles/mapbox/dark-v9',
+      sgCenter: [103.82161, 1.35433],
+    };
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
